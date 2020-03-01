ed89d4c66feeab9c227c186cdefae3350fbea2e1
rizaldodo <rizal.dodo@rocketmail.com>
2020-03-02 01:18:15 +0700

    add squares

---
+++ colorGame.js
@@ -0,0 +1,27 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay")
+
+for(let i=0; i<squares.length; i++){
+    squares[i].style.background = colors[i];
+
+    squares[i].addEventListener("click", function(){
+        //grab color of clicked square
+        clickedColor = this.style.background;
+        //compare color
+        if(clickedColor === pickedColor){
+            alert("Correct!");
+        }else{
+            alert("Wrong!");
+        }
+    });
+}
